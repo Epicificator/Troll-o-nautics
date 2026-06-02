d2729dcd3c82da59937a270d284d4b58840246c4
Wenbell <80232435+OnARiverMadeOfEyes@users.noreply.github.com>
2026-06-02 16:41:12 +0000

    replace cdg oil with tmfg oil
    also adjusted cdg distillation

---
+++ kubejs/server_scripts/oil.js
@@ -0,0 +1,32 @@
+ServerEvents.recipes(event => {
+    event.remove({ id: 'createdieselgenerators:distillation/crude_oil' })
+    event.replaceInput({ id: 'createdieselgenerators:crafting/asphalt_block' }, 'createdieselgenerators:crude_oil_bucket', Ingredient.of('tfmg:crude_oil_bucket'))
+
+    event.remove({ id: 'createdieselgenerators:distillation/crude_oil' })
+    event.recipes.createdieselgenerators
+    .distillation([
+        Fluid.of("tfmg:diesel", 45),
+        Fluid.of("tfmg:gasoline", 35),
+        ],
+        Fluid.of("tfmg:crude_oil", 100))
+    .processingTime(60)
+    .heated();
+    event.remove({ id: 'createdieselgenerators:distillation/superheated_crude_oil' })
+    event.recipes.createdieselgenerators
+    .distillation([
+        Fluid.of("tfmg:diesel", 25),
+        Fluid.of("tfmg:gasoline", 55),
+        ],
+        Fluid.of("tfmg:crude_oil", 100))
+    .processingTime(40)
+    .superheated();
+})
+
+RecipeViewerEvents.removeEntries('fluid', event => {
+    event.remove('createdieselgenerators:crude_oil')
+    event.remove('createdieselgenerators:gasoline')
+    event.remove('createdieselgenerators:diesel')
+})
+RecipeViewerEvents.removeEntries('item', event => {
+    event.remove('createdieselgenerators:crude_oil_bucket')
+})
