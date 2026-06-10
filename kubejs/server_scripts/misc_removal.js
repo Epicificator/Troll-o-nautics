1ec2d1a50d20127e9727b9328b69563b3fccaf77
Wenbell <Wenbell@unknown088fc36cddbe.attlocal.net>
2026-06-10 03:54:31 -0500

    replace platinum with palladium + remvove cc turts

--- kubejs/server_scripts/misc_removal.js
+++ kubejs/server_scripts/misc_removal.js
@@ -1,3 +1,10 @@
+ServerEvents.recipes(event => {
+    event.remove({ output: 'computercraft:turtle_normal' })
+    event.remove({ output: 'computercraft:turtle_advanced' })
+})
+
 RecipeViewerEvents.removeEntries('item', event => {
+    event.remove('computercraft:turtle_normal')
+    event.remove('computercraft:turtle_advanced')
     event.remove('lootr:trophy')
 })
