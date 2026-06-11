6dd235c8c3490759514b587e1197a41484f7a03d
Wenbell <80232435+OnARiverMadeOfEyes@users.noreply.github.com>
2026-06-11 17:07:58 +0000

    half charcoal burn

--- kubejs/startup_scripts/item.js
+++ kubejs/startup_scripts/item.js
@@ -1,3 +1,9 @@
 StartupEvents.registry('item', event => {
     event.create('blank_disc')
 })
+
+ItemEvents.modification(event => {
+    event.modify('minecraft:charcoal', item => {
+        item.burnTime = 800
+    })
+})
