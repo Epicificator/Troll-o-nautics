3f90d223d34853aea4e1fbf1ed13afb25b36fac1
Glytch10 <76640594+Glytch10@users.noreply.github.com>
2026-06-18 16:15:04 -0500

    wayfinder and vaults

--- kubejs/server_scripts/misc_removal.js
+++ kubejs/server_scripts/misc_removal.js
@@ -1,6 +1,8 @@
 ServerEvents.recipes(event => {
     event.remove({ output: 'computercraft:turtle_normal' })
     event.remove({ output: 'computercraft:turtle_advanced' })
+    event.remove('wayfinder:signal_array')
+    event.remove('wayfinder:signalscope')
 })
 
 RecipeViewerEvents.removeEntries('item', event => {
