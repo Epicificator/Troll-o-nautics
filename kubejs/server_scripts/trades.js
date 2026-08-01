b4af3df6440d088cdaf73811fb1dc2830ae1803a
Wenbell <80232435+OnARiverMadeOfEyes@users.noreply.github.com>
2026-08-01 18:59:20 +0000

    Update trades.js

--- kubejs/server_scripts/trades.js
+++ kubejs/server_scripts/trades.js
@@ -16,7 +16,7 @@
         }
         //         profession,            maxuses   lvl  product                min  max    silver
         drugtrade('minecraft:armorer',          3,  1,  'nirvana:weed_brownie',  8,   16,    1)
-        drugtrade('minecraft:cartographer',     2,  3,  'createbb:sudafed',      8,   16,    2)
+        drugtrade('minecraft:cartographer',     2,  3,  'createbb:sudafed_box',      8,   16,    2)
         drugtrade('minecraft:cleric',           1,  3,  'createbb:white_meth',   4,   8,     2)
         drugtrade('minecraft:cleric',           1,  4,  'createbb:blue_meth',    8,   16,    3)
         drugtrade('minecraft:cleric',           4,  1,  'nirvana:weed',          32,  48,    1)
@@ -24,10 +24,10 @@
         drugtrade('minecraft:farmer',           2,  1,  'nirvana:weed_brownie',  8,   16,    1)
         drugtrade('minecraft:fletcher',         1,  1,  'nirvana:weed',          32,  48,    1)
         drugtrade('minecraft:leatherworker',    1,  4,  'createbb:blue_meth',    4,   8,     5)
-        drugtrade('minecraft:librarian',        2,  3,  'createbb:sudafed',      8,   16,    2)
+        drugtrade('minecraft:librarian',        2,  3,  'createbb:sudafed_box',      8,   16,    2)
         drugtrade('minecraft:librarian',        1,  1,  'nirvana:weed_brownie',  8,   16,    1)
         drugtrade('minecraft:shepherd',         2,  3,  'createbb:white_meth',   4,   8,     2)
-        drugtrade('minecraft:toolsmith',        1,  3,  'createbb:sudafed',      8,   16,    2)
+        drugtrade('minecraft:toolsmith',        1,  3,  'createbb:sudafed_box',      8,   16,    2)
         drugtrade('minecraft:toolsmith',        2,  1,  'nirvana:weed_brownie',  8,   16,    1)
         drugtrade('minecraft:weaponsmith',      2,  4,  'createbb:blue_meth',    4,   8,     4)
 })
